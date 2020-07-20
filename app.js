e50bc97186b51e4c365deab01b2f9653b330d910
yousefShallah <yousefmohshallah33@gmail.com>
2020-07-20 11:15:52 +0300

    yousef [add] create new graphql with node js project

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const bodyParser = require('body-parser')
+const { buildSchema } = require('graphql');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/graphql',  
+        graphqlHTTP({
+            schema: buildSchema(`
+                type RootQuery {
+                    events: [String!]!
+                }
+                type RootMutation {
+                    createEvent(name: String): String
+                }
+                schema {
+                    query: RootQuery
+                    mutation: RootMutation
+                }  
+            `), 
+            rootValue: {
+                events: () => {
+                    return ['Romantic ', 'Sailing', ' Coding'];
+                },
+                createEvent: (args) => {
+                    const eventName = args.name;
+                    return eventName;
+                }
+            },
+            graphiql: true
+        }   
+    )
+);
+
+app.listen(PORT)
