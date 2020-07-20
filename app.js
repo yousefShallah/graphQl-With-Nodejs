9ae60277e5d79ec83d393fa27873265722aee117
yousefShallah <yousefmohshallah33@gmail.com>
2020-07-20 13:03:38 +0300

    yousef [add] edit

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ app.use('/graphql',
         graphqlHTTP({
             schema: buildSchema(`
                 type RootQuery {
-                    events: [String!]!
+                    event: [String!]!
                 }
+                
                 type RootMutation {
                     createEvent(name: String): String
+                    deleteEvent(id:Int, name: String): String
                 }
                 schema {
                     query: RootQuery
@@ -24,12 +26,22 @@ app.use('/graphql',
                 }  
             `), 
             rootValue: {
-                events: () => {
-                    return ['Romantic ', 'Sailing', ' Coding'];
+                event: () => {
+                    const data_json = [ { name: "yousef", age: '19', agp: '120' }, { name: "mohammed", age: '22', agp: '60' }, { name: "khalel", age: '24', agp: '88' } ]
+                    const names = data_json.map(data => {
+                        const alldata = data.name;
+                        return alldata;
+                    });
+                    return names;
                 },
                 createEvent: (args) => {
                     const eventName = args.name;
                     return eventName;
+                }, 
+                deleteEvent: (args) =>{
+                    const id_user = args.id;
+                    const name = args.name;
+                    return `delete user Id: ${id_user}>>> Name: ${name}`;
                 }
             },
             graphiql: true
